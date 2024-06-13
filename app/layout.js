8f8e63fbadd15ddc684e721bc1449e3021299230
Anek Porwal (Gokwik) <anek.porwal@gokwik.co>
2024-06-14 03:09:38 +0530

    Navbar & LoginForm Added

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
 import { Inter } from "next/font/google";
+import { Providers } from "./providers";
+import Navbar  from "./components/NavbarMenu";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,7 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>
+          <Navbar>
+          </Navbar>
+          {children}
+          </Providers>
+      </body>
     </html>
   );
 }
